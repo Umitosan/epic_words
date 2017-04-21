80cbe374eb7e4167363e2b1c82f81d14bbaee173
Patrick Leahy <tenebrousedge@gmail.com>
2017-04-21 13:19:26 -0700

    add spec for comparison class

--- spec/epic_words_spec.rb
+++ spec/epic_words_spec.rb
@@ -6,6 +6,35 @@ RSpec.describe EpicWords do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
+  end
+
+  describe 'EpicWords#compare' do
+    it 'returns an error if both words are not words' do
+      expect(EpicWords.compare('blarp', 'poit')).to match(/Error:/)
+    end
+
+    it 'if given two anagrams, return a string which says they are anagrams' do
+      expect(EpicWords.compare('cat', 'act')).to match(/are anagrams/)
+    end
+
+    it 'if given two valid words which are not anagrams, return a string message to that effect' do
+      expect(EpicWords.compare('cat', 'art')).to match(/not anagrams/)
+    end
+
+    it 'will match anagrams case-insensitively' do
+      expect(EpicWords.compare('cat', 'ACT')).to match(/are anagrams/)
+    end
+
+    it 'will indicate if words are antigrams' do
+      expect(EpicWords.compare('cat', 'ruby')).to match(/are antigrams/)
+    end
+
+    it 'will indicate whether the words are palindromes' do
+      expect(EpicWords.compare('racecar', 'racecar')).to match(/palindrome/)
+    end
+
+    it 'will match multiple words as being antigrams or anagrams' do
+      expect(EpicWords.compare('Internet Anagram Server', 'I, Rearrangement Servant')).to match('/are anagrams/')
   end
 end
