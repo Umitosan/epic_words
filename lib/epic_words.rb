3ecce71ed5580301158a60152ce1104b4f2d2a63
Patrick Leahy <tenebrousedge@gmail.com>
2017-04-21 14:13:57 -0700

    add code to make test cases pass
    deleted version file
    TODO: figure out something with multi-word things

--- lib/epic_words.rb
+++ lib/epic_words.rb
@@ -1,5 +1,26 @@
-require "epic_words/version"
+require 'epic_anagram'
+require 'set'
 
-module EpicWords
-  # Your code goes here...
+class EpicWords
+  def compare(first, second)
+  	if (first.include?(' ') || second.include?(' '))
+  		#special case it.
+  	end
+  	string1, string2 = [first,second].map {|s| s.downcase.gsub(/\W+/, '')}
+  	message = ''
+  	message << "These words are palindromes.\n" if are_palindromes?(string1, string2)
+  	message << "These words are antigrams.\n" if are_antigrams?(string1, string2)
+  	message << "These words are anagrams\n" if EpicAnagram.find_anagrams(string1).include?(string2)
+  	message.empty? ? 'Error: these words are not anagrams, palindromes, or antigrams' : message
+  end
+
+  def are_antigrams?(first, second)
+  	[first, second].map {|e| Set.new(e.split(''))}.reduce(&:&).empty?
+  end
+
+  def are_palindromes?(first, second)
+  	first.reverse == second
+  end
+
+
 end
