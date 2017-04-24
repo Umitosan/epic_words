64e6f0bd2d6e78d909a741228b5b8905e51b7796
Patrick Leahy <tenebrousedge@gmail.com>
2017-04-23 17:01:26 -0700

    Adding documentation and rewrite functions as a module

--- lib/epic_words.rb
+++ lib/epic_words.rb
@@ -1,37 +1,78 @@
 require 'epic_anagram'
 require 'set'
 
+# This class allows people to do fun things with words
+# It should maybe be a module instead
 class EpicWords
-  def compare(first, second)
-    message = ''
-    string1, string2 = [first,second].map {|s| s.downcase.gsub(/\W+/, '')}
-    message << "These words are palindromes.\n" if are_palindromes?(string1, string2)
-    message << "These words are antigrams.\n" if are_antigrams?(string1, string2)
-    if are_anagrams?(string1, string2)
-      message << "These words are anagrams\n" 
+  # This module exists because it's slightly more readable to type 
+  # strings.are_palindromes?
+  # rather than 
+  # are_palindromes?(strings)
+  module WordFunctions
+    # compares an array of words to see if they are all antigrams
+    # @return [Boolean]
+    def are_antigrams?
+      # map/reduce strikes again
+      # Set is the proper class to use for this because it will automagically eliminate duplicate characters
+      # the & operator returns the set intersection
+      # if the set intersection is empty the words share no characters
+      self.map {|s| Set.new(s.split(''))}.reduce(&:&).empty?
     end
-    message.empty? ? 'Error: these words are not anagrams, palindromes, or antigrams' : message
+
+    # compares an array of words to see if they are all palindromes
+    #
+    # this could have been written to test if all strings are palindromes of each other, but this is easier
+    # @return [Boolean]
+    def are_palindromes?
+      self.all? {|s| s == s.reverse }
+    end
+
+    # compares an array of words to see if they are all anagrams of each other
+    # @return [Boolean]
+    def are_anagrams?
+      # for all inputs, split them to an array, and return an array of the sorted characters
+      # then find out how many unique arrays we have
+      # the words are all anagrams IFF we have an array of length 1
+      self.map { |s| s.split('').sort}.uniq.length == 1
+    end
   end
 
-  def are_antigrams?(*args)
-    # map/reduce strikes again
-    # Set is the proper class to use for this
-    # the & operator returns the set intersection
-    # if the set intersection is empty the words share no characters
-    args.map {|e| Set.new(e.split(''))}.reduce(&:&).empty?
+  # Compares strings to see if they are palindromes, antigrams, or anagrams
+  #
+  # 
+  # @param [Array<String>] *strings Any number of strings to compare
+  # @return [String]
+  def compare(*strings)
+    # downcases all strings and strips out non-alphanumeric characters
+    strings.map! {|s| s.downcase.gsub(/\W+/, '')}
+    # 
+    strings.extend(WordFunctions)
+    # we use the case statement as a shorthand way of writing a bunch of if/else statements
+    # ruby is interesting in that:
+    # 1. conditional statements return expressions
+    # 2. case statements don't need to evaluate anything when initially declared
+    types = [] << case
+    when strings.are_palindromes?
+      "palindromes"
+    when strings.are_antigrams?
+      "antigrams"
+    when strings.are_anagrams?
+      "anagrams"
+    end
+    # any? as opposed to empty? because case will return nil by default
+    if types.any?
+      # writing it like this avoids duplicating the string
+      types.reduce('') {|acc, e| acc += "These words are #{e}\n"}
+    else
+      'Error: these words are not anagrams, palindromes, or antigrams'
+    end
   end
 
-  def are_palindromes?(first, second)
-    first.reverse == first && second.reverse == second
-  end
-
-  def are_anagrams?(*args)
-    # for all inputs, split them to an array, and return an array of the sorted characters
-    # then find out how many unique arrays we have
-    # the words are all anagrams IFF we have an array of length 1
-    args.map { |e| e.split('').sort}.uniq.length == 1
-  end
-
+  # finds anagrams for a given word. Function provided by the EpicAnagram gem
+  #
+  # @link https://github.com/tenebrousedge/epic_anagram
+  # @param [String] word A word to find anagrams for
+  # @return [String]
   def anagrams(word)
     EpicAnagram.find_anagrams(word)
   end
